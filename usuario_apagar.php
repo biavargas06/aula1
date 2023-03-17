cedd64decf15bf3fb16339a9fa5363df8ed57a0e
Bianca Vargas <125467308+biavargas06@users.noreply.github.com>
2023-03-17 11:08:21 -0300

    apagar usuário

--- usuario_apagar.php
+++ usuario_apagar.php
@@ -1,4 +1,24 @@
 <?php
- require('twig_carregar.php');
+require('twig_carregar.php');
+require('pdo.inc.php');//Conexão com o banco
 
- echo $twig->render('usuario_apagar.html');
+//Rotina de POST - Apagar usr
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $id = $_POST['id'] ?? false;
+    if($id){
+          $sql = $pdo->prepare('UPDATE usuarios SET ativo = 0 WHERE id= ?');
+          $sql->execute([$id]);
+    }
+
+    header('location:usuarios.php');
+    die;
+}
+//Rotina de GET - Mostrar info na tela
+//Buscar usuário no banco
+$id = $_GET['id'] ?? false;
+$sql = $pdo->prepare('SELECT * FROM usuarios WHERE id = ?');
+$sql->execute([$id]);
+$usuario = $sql->fetch(PDO::FETCH_ASSOC);
+
+ echo $twig->render('usuario_apagar.html',['usuario'=> $usuario,
+ ]);
